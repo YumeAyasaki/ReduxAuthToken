3a82e80d80d63ceb492b0d87a9a427e97b8fe878
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2024-10-09 11:10:10 +0700

    Minor update.

--- fe/app/login/page.tsx
+++ fe/app/login/page.tsx
@@ -155,7 +155,7 @@ export default function Login() {
       {auth.isAuth && (
         <div className="flex h-screen bg-black">
           <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-white">
-            You are logged in as
+            You are logged in as {auth.username}
             <SignOut />
           </div>
         </div>
